600e29978456ce88646b7f8437908a1e52c42e88
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-09-28 03:09:32 +0900

    feat: オプションのマイグレーションファイル #1

---
+++ db/migrate/20250928025923-create_options.sql
@@ -0,0 +1,13 @@
+-- オプション
+-- +migrate Up
+CREATE TABLE options(
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- コメント
+COMMENT ON COLUMN options.id IS 'オプションID';
+COMMENT ON COLUMN options.name IS 'オプション名';
+
+-- +migrate Down
+DROP TABLE IF EXISTS options; -- テーブルが存在する場合のみ削除する
