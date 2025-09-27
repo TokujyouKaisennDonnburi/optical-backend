0a8809b8418a4c7004f38a4881887ee0e19de98d
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-09-28 03:35:07 +0900

    feat: eventのマイグレーションファイル作成 #1

---
+++ db/migrate/20250928030023-create_event.sql
@@ -0,0 +1,37 @@
+-- 予定のテーブル
+
+-- +migrate Up
+CREATE TABLE event(
+    id UUID PRIMARY KEY,
+    schedule_id UUID REFERENCES schedules(id) ON DELETE CASCADE,
+    title VARCHAR(255) NOT NULL,
+    memo TEXT,
+    color VARCHAR(50),
+    all_day BOOLEAN,
+    start_at TIMESTAMP NOT NULL,
+    end_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP NULL   -- NULL許容
+);
+
+-- コメント
+COMMENT ON COLUMN event.id IS 'イベントID';
+COMMENT ON COLUMN event.schedule_id IS 'スケジュールID';
+COMMENT ON COLUMN event.title IS 'タイトル';
+COMMENT ON COLUMN event.memo IS 'メモ';
+COMMENT ON COLUMN event.color IS 'カラー';
+COMMENT ON COLUMN event.all_day IS '終日フラグ';
+COMMENT ON COLUMN event.start_at IS '開始時刻';
+COMMENT ON COLUMN event.end_at IS '終了時刻';
+COMMENT ON COLUMN event.created_at IS '作成日時';
+COMMENT ON COLUMN event.updated_at IS '更新日時';
+COMMENT ON COLUMN event.deleted_at IS '削除日時';
+
+-- updated_atを自動更新するトリガーを作成
+SELECT create_updated_at_trigger('event');
+
+
+-- +migrate Down
+DROP TRIGGER IF EXISTS update_event_updated_at ON event; -- トリガーも削除
+DROP TABLE IF EXISTS event; -- テーブルが存在する場合のみ削除する
