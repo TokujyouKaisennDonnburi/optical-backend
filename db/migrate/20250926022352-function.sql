943886f19f4a23cbe21eeb86ca877e2277f6a734
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-09-28 03:29:19 +0900

    refactor: トリガー作成関数を作成 #1

--- db/migrate/20250926022352-function.sql
+++ db/migrate/20250926022352-function.sql
@@ -12,7 +12,19 @@ BEGIN
 END;
 $$ LANGUAGE 'plpgsql';
 
-
+-- テーブルごとにトリガーを作成する関数
+CREATE OR REPLACE FUNCTION create_updated_at_trigger(table_name TEXT)
+RETURNS VOID AS $$
+BEGIN
+    EXECUTE format('
+        CREATE TRIGGER update_%I_updated_at
+            BEFORE UPDATE ON %I
+            FOR EACH ROW
+            EXECUTE FUNCTION update_updated_at_column();
+    ', table_name, table_name);
+END;
+$$ LANGUAGE 'plpgsql';
 
 -- +migrate Down
+DROP FUNCTION IF EXISTS create_updated_at_trigger(TEXT);
 DROP FUNCTION IF EXISTS update_updated_at_column();
