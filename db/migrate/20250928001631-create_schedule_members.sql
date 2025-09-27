0cf787fb458be9b2c51fdc8d907a4a78cd4f35a2
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-09-28 02:33:52 +0900

    update: 参加日忘れてたのを追加 #1

--- db/migrate/20250928001631-create_schedule_members.sql
+++ db/migrate/20250928001631-create_schedule_members.sql
@@ -4,12 +4,14 @@
 CREATE TABLE schedule_members(
     schedule_id UUID REFERENCES schedules(id) ON DELETE CASCADE,
     user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- NULL許容
     PRIMARY KEY (schedule_id, user_id)  -- 複合主キー
 );
 
 -- コメント
 COMMENT ON COLUMN schedule_members.schedule_id IS 'スケジュールID';
 COMMENT ON COLUMN schedule_members.user_id IS 'ユーザーID';
+COMMENT ON COLUMN schedule_members.joined_at IS '参加日時';
 
 -- +migrate Down
 DROP TABLE IF EXISTS schedule_members; -- テーブルが存在する場合のみ削除する
