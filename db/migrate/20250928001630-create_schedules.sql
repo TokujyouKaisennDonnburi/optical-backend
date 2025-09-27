8fcc8eae4e4ee9cfd131064571f2c1793dee3237
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-09-28 02:58:54 +0900

    feat: スケジュールのマイグレーションファイル作成 #1

---
+++ db/migrate/20250928001630-create_schedules.sql
@@ -0,0 +1,16 @@
+-- スケジュールのマイグレーションファイル
+
+-- +migrate Up
+CREATE TABLE schedules(
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    deleted_at TIMESTAMP NULL   -- NULL許容
+)
+
+-- コメント
+COMMENT ON COLUMN schedules.id IS 'スケジュールID';
+COMMENT ON COLUMN schedules.name IS 'スケジュール名';
+COMMENT ON COLUMN schedules.deleted_at IS '削除日時';
+
+-- +migrate Down
+DROP TABLE IF EXISTS schedules; -- テーブルが存在する場合のみ削除する
