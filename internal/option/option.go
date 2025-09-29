9980c8240677b374f3a26b65afd96460214a4d6f
yonagi <lecterkn@proton.me>
2025-09-29 21:08:21 +0900

    feat: オプションのドメインモデルを実装 #8

--- internal/option/option.go
+++ internal/option/option.go
@@ -1,4 +1,25 @@
 package option
 
-// TODO issue#8
-type Option struct {}
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type Option struct {
+	id   uuid.UUID
+	name string
+}
+
+func NewOption(id uuid.UUID, name string) (*Option, error) {
+	if id == uuid.Nil {
+		return nil, errors.New("Option `id` is nil")
+	}
+	if name == "" {
+		return nil, errors.New("Option `name` is nil")
+	}
+	return &Option{
+		id:   id,
+		name: name,
+	}, nil
+}
