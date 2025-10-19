c5beb6133ff486fde7547838ed53f11ec6656b87
mame77 <morinaga.kota@icloud.com>
2025-10-19 20:50:06 +0900

    calendarのapeend関数を変更 #47

--- internal/schedule/calendar.go
+++ internal/schedule/calendar.go
@@ -2,11 +2,5 @@ package schedule
 
 type Calendar []Event
 
-func (c *Calendar) append(event Event) Calendar {
-	events := append(c.events(), event)
-	return Calendar(events)
-}
-
-func (c *Calendar) events() []Event {
-	return *c
-}
+func (c Calendar) append(event Event) Calendar {
+    return append(c, event)
