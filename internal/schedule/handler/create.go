aee72988d3b62779ffb5db47935c5cca2c8c9ccc
yonagi <lecterkn@proton.me>
2025-10-29 21:10:36 +0900

    refactor: エラーハンドリングとlintの修正 #11

--- internal/schedule/handler/create.go
+++ internal/schedule/handler/create.go
@@ -24,21 +24,27 @@ type ScheduleCreateResponse struct {
 func (h *ScheduleHttpHandler) Create(w http.ResponseWriter, r *http.Request) {
 	userId, err := uuid.Parse(r.Context().Value("id").(string))
 	if err != nil {
-		render.Render(w, r, apperr.ErrInternalServerError(err))
+		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
 		return
 	}
 	var request ScheduleCreateRequest
 	// リクエストJSONをバインド
 	err = json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-		render.Render(w, r, apperr.ErrInvalidRequest(err))
+		err = render.Render(w, r, apperr.ErrInvalidRequest(err))
+		if err != nil {
+			_ = render.Render(w, r, apperr.ErrInternalServerError(err))
+		}
 		return
 	}
 	optionIds := []uuid.UUID{}
 	for _, id := range request.OptionIds {
 		optionId, err := uuid.Parse(id)
 		if err != nil {
-			render.Render(w, r, apperr.ErrInvalidRequest(err))
+			err = render.Render(w, r, apperr.ErrInvalidRequest(err))
+			if err != nil {
+				_ = render.Render(w, r, apperr.ErrInternalServerError(err))
+			}
 			return
 		}
 		optionIds = append(optionIds, optionId)
@@ -50,7 +56,7 @@ func (h *ScheduleHttpHandler) Create(w http.ResponseWriter, r *http.Request) {
 		OptionIds:    optionIds,
 	})
 	if err != nil {
-		render.Render(w, r, apperr.ErrInternalServerError(err))
+		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
 		return
 	}
 	// レスポンスに変換
