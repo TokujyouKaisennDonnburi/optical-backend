8afe4ee85ec8357063238ac2dcf96a7abb83e157
yonagi <lecterkn@proton.me>
2025-10-30 20:42:52 +0900

    feat: ユーザー新規作成APIをサーバーに追加 #15

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+
+	userGateway "github.com/TokujouKaisenDonburi/optical-backend/internal/user/gateway"
+	userHandler "github.com/TokujouKaisenDonburi/optical-backend/internal/user/handler"
+	userCommand "github.com/TokujouKaisenDonburi/optical-backend/internal/user/service/command"
+	"github.com/jmoiron/sqlx"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func init() {
@@ -15,8 +23,49 @@ func init() {
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
+
+	db := getPostgresDB()
+	userRepository := userGateway.NewUserPsqlRepository(db)
+	userCommand := userCommand.NewUserCommand(userRepository)
+	userHandler := userHandler.NewUserHttpHandler(userCommand)
+
+	// Users
+	r.Post("/register", userHandler.Create)
+
+	// Start Serving
 	http.ListenAndServe(":8000", r)
+}
+
+// PostgresのDBに接続する
+func getPostgresDB() *sqlx.DB {
+	// #63 環境変数から読むように変える
+	host := "127.0.0.1"
+	port := "5433"
+	sslMode := "disable"
+	user, ok := os.LookupEnv("POSTGRES_USER")
+	if !ok {
+		panic("'POSTGRES_USER' is not set")
+	}
+	pass, ok := os.LookupEnv("POSTGRES_PASSWORD")
+	if !ok {
+		panic("'POSTGRES_PASSWORD' is not set")
+	}
+	dbName, ok := os.LookupEnv("POSTGRES_DB")
+	if !ok {
+		panic("'POSTGRES_DB' is not set")
+	}
+	dsn := fmt.Sprintf(
+		"host=%s port=%s database=%s user=%s password=%s sslmode=%s",
+		host,
+		port,
+		dbName,
+		user,
+		pass,
+		sslMode,
+	)
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.4
 
 require (
 	github.com/go-chi/chi/v5 v5.2.3
+	github.com/go-chi/render v1.0.3
 	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1
@@ -12,6 +13,5 @@ require (
 
 require (
 	github.com/ajg/form v1.5.1 // indirect
-	github.com/go-chi/render v1.0.3 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 )
