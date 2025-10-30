05ae4aa2aa63aee12ce3eb3d8009d9d963bfad39
yonagi <lecterkn@proton.me>
2025-10-31 02:40:10 +0900

    feat: ユーザをメールから検索して取得する #16

--- internal/user/gateway/user.go
+++ internal/user/gateway/user.go
@@ -2,8 +2,10 @@ package gateway
 
 import (
 	"context"
+	"time"
 
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/user"
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -20,16 +22,26 @@ func NewUserPsqlRepository(db *sqlx.DB) *UserPsqlRepository {
 	}
 }
 
+type UserModel struct {
+	Id        uuid.UUID `db:"id"`
+	Name      string    `db:"name"`
+	Email     string    `db:"email"`
+	Password  []byte    `db:"password_hash"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+	DeletedAt time.Time `db:"deleted_at"`
+}
+
 func (r *UserPsqlRepository) Create(ctx context.Context, user *user.User) error {
 	query := `
 		INSERT INTO users(id, name, email, password_hash, created_at, updated_at)
 		VALUES(:id, :name, :email, :password, :createdAt, :updatedAt)
 	`
 	_, err := r.db.NamedExecContext(ctx, query, map[string]any{
-		"id": user.Id,
-		"name": user.Name,
-		"email": user.Email,
-		"password": user.Password,
+		"id":        user.Id,
+		"name":      user.Name,
+		"email":     user.Email,
+		"password":  user.Password,
 		"createdAt": user.CreatedAt,
 		"updatedAt": user.UpdatedAt,
 	})
@@ -37,4 +49,28 @@ func (r *UserPsqlRepository) Create(ctx context.Context, user *user.User) error 
 		return err
 	}
 	return nil
+}
+
+func (r *UserPsqlRepository) FindByEmail(ctx context.Context, email string) (*user.User, error) {
+	query := `
+		SELECT 
+			id, name, email, password_hash, created_at, updated_at, deleted_at
+		FROM users
+		WHERE 
+			email = ?
+	`
+	userModel := UserModel{}
+	err := r.db.Get(&userModel, query, email)
+	if err != nil {
+		return nil, err
+	}
+	return &user.User{
+		Id: userModel.Id,
+		Name: userModel.Name,
+		Email: userModel.Email,
+		Password: userModel.Password,
+		CreatedAt: userModel.CreatedAt,
+		UpdatedAt: userModel.UpdatedAt,
+		DeletedAt: userModel.DeletedAt,
+	}, nil
 }
--- internal/user/repository/user.go
+++ internal/user/repository/user.go
@@ -8,5 +8,6 @@ import (
 
 type UserRepository interface {
 	Create (ctx context.Context, user *user.User) error
+	FindByEmail(ctx context.Context, email string) (*user.User, error)
 }
 
