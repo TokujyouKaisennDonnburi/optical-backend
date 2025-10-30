0768bd819a0698811b5d7c93325c9ff6d761e44d
yonagi <lecterkn@proton.me>
2025-10-30 18:24:44 +0900

    add: CreateScheduleのコンストラクタ #11

--- internal/schedule/service/command/command.go
+++ internal/schedule/service/command/command.go
@@ -10,3 +10,16 @@ type ScheduleCommand struct {
 	optionRepository   optionRepo.OptionRepository
 }
 
+func NewScheduleCommand(scheduleRepository scheduleRepo.ScheduleRepository, optionRepository optionRepo.OptionRepository) *ScheduleCommand {
+	if scheduleRepository == nil {
+		panic("scheduleRepository is nil")
+	}
+	if optionRepository == nil {
+		panic("optionRepository is nil")
+	}
+	return &ScheduleCommand{
+		scheduleRepository: scheduleRepository,
+		optionRepository: optionRepository,
+	}
+}
+
