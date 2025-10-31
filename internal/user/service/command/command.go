8ee04313d44f4ce4ae5a0eeee86663736c96db37
yonagi <lecterkn@proton.me>
2025-10-31 10:23:45 +0900

    feat: ユーザーログインのコマンドを実装 #16

--- internal/user/service/command/command.go
+++ internal/user/service/command/command.go
@@ -4,13 +4,18 @@ import "github.com/TokujouKaisenDonburi/optical-backend/internal/user/repository
 
 type UserCommand struct {
 	userRepository repository.UserRepository
+	tokenRepository repository.TokenRepository
 }
 
-func NewUserCommand(userRepository repository.UserRepository) *UserCommand {
+func NewUserCommand(userRepository repository.UserRepository, tokenRepository repository.TokenRepository) *UserCommand {
 	if userRepository == nil {
 		panic("UserRepository is nil")
 	}
+	if tokenRepository == nil {
+		panic("TokenRepository is nil")
+	}
 	return &UserCommand{
 		userRepository: userRepository,
+		tokenRepository: tokenRepository,
 	}
 }
