0082be7ed75bb495f9e732272c292a93b781a15f
yonagi <lecterkn@proton.me>
2025-10-31 10:23:45 +0900

    feat: ユーザーログインのコマンドを実装 #16

---
+++ internal/user/service/command/login_user.go
@@ -0,0 +1,52 @@
+package command
+
+import (
+	"context"
+	"errors"
+
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/user"
+)
+
+type UserLoginInput struct {
+	Email    string
+	Password string
+}
+
+type UserLoginOutput struct {
+	AccessToken  string
+	RefreshToken string
+}
+
+// ユーザーにログインする
+func (c *UserCommand) LoginUser(ctx context.Context, input UserLoginInput) (*UserLoginOutput, error) {
+	// ユーザー取得
+	loginUser, err := c.userRepository.FindByEmail(ctx, input.Email)
+	if err != nil {
+		return nil, err
+	}
+	if loginUser.IsDeleted() {
+		return nil, errors.New("The user is deleted")
+	}
+	if !loginUser.VerifyPassword(input.Password) {
+		return nil, errors.New("password is incorrect")
+	}
+	// アクセストークン発行
+	accessToken, err := user.NewAccessToken(loginUser)
+	if err != nil {
+		return nil, err
+	}
+	// リフレッシュトークン発行
+	refreshToken, err := user.NewRefreshToken(loginUser)
+	if err != nil {
+		return nil, err
+	}
+	// リフレッシュトークンを保存
+	err = c.tokenRepository.AddToWhitelist(refreshToken)
+	if err != nil {
+		return nil, err
+	}
+	return &UserLoginOutput{
+		AccessToken: accessToken.Token,
+		RefreshToken: refreshToken.Token,
+	}, nil
+}
