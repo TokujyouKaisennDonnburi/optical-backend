9a4be5826a85f5573d699c20c8e0e7180fb2a61f
yonagi <lecterkn@proton.me>
2025-10-31 11:21:28 +0900

    feat: 自身のユーザー情報を取得するハンドラ #16

--- internal/user/handler/handler.go
+++ internal/user/handler/handler.go
@@ -1,16 +1,24 @@
 package handler
 
-import "github.com/TokujouKaisenDonburi/optical-backend/internal/user/service/command"
+import (
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/user/service/command"
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/user/service/query"
+)
 
 type UserHttpHandler struct {
+	userQuery   *query.UserQuery
 	userCommand *command.UserCommand
 }
 
-func NewUserHttpHandler(userCommand *command.UserCommand) *UserHttpHandler {
+func NewUserHttpHandler(userQuery *query.UserQuery, userCommand *command.UserCommand) *UserHttpHandler {
+	if userQuery == nil {
+		panic("UserQuery is nil")
+	}
 	if userCommand == nil {
 		panic("UserCommand is nil")
 	}
 	return &UserHttpHandler{
+		userQuery:   userQuery,
 		userCommand: userCommand,
 	}
 }
