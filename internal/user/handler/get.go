ec4552a17d9b031e528a04f02e65b4bed2d36c62
yonagi <lecterkn@proton.me>
2025-10-31 11:21:28 +0900

    feat: 自身のユーザー情報を取得するハンドラ #16

---
+++ internal/user/handler/get.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/user/service/query"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/apperr"
+	"github.com/go-chi/render"
+	"github.com/google/uuid"
+)
+
+type UserResponse struct {
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+// 自身のユーザー情報を取得する
+func (h *UserHttpHandler) GetMe(w http.ResponseWriter, r *http.Request) {
+	// IDを取得
+	userId, err := uuid.Parse(r.Context().Value("id").(string))
+	if err != nil {
+		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
+		return
+	}
+	// ユーザー情報を取得
+	output, err := h.userQuery.GetUser(r.Context(), query.UserQueryInput{
+		UserId: userId,
+	})
+	if err != nil {
+		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
+	}
+	render.JSON(w, r, UserResponse{
+		Id: output.Id.String(),
+		Name: output.Name,
+		Email: output.Email,
+		CreatedAt: output.CreatedAt,
+		UpdatedAt: output.UpdatedAt,
+	})
+}
