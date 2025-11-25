18cea8856b89496cd3bc5e8f020c0bfe4f313f8e
yonagi <lecterkn@proton.me>
2025-11-25 12:13:37 +0900

    add: 予定の場所をデータベースに追加 #77

---
+++ db/migrate/20251125120347-add_locations_to_events.sql
@@ -0,0 +1,15 @@
+
+-- +migrate Up
+CREATE TABLE event_locations(
+    event_id UUID NOT NULL UNIQUE REFERENCES events(id) ON DELETE CASCADE,
+    location VARCHAR(255) NOT NULL,
+)
+
+CREATE UNIQUE INDEX idx_event_locations_event_id_and_location ON event_locations(event_id, location);
+CREATE INDEX idx_event_locations_event_id ON event_locations(event_id);
+
+-- +migrate Down
+
+DROP INDEX IF EXISTS idx_event_locations_event_id_and_location 
+DROP INDEX IF EXISTS idx_event_locations_event_id;
+DROP TABLE IF EXISTS event_locaitons;
