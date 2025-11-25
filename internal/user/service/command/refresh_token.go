21a09c96f67f6a962dd3550b603ea24df98936bf
yonagi <lecterkn@proton.me>
2025-11-25 11:44:41 +0900

    add: トークンリフレッシュ用コマンド #72

---
+++ internal/user/service/command/refresh_token.go
@@ -0,0 +1,40 @@
+package command
+
+import (
+	"context"
+	"time"
+
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/user"
+)
+
+type TokenRefreshInput struct {
+	RefreshToken string
+}
+
+type TokenRefreshOutput struct {
+	AccessToken string
+	ExpiresIn   time.Time
+}
+
+// リフレッシュトークンを使用してアクセストークンを発行する
+func (u UserCommand) Refresh(ctx context.Context, input TokenRefreshInput) (*TokenRefreshOutput, error) {
+	// リフレッシュトークンの情報を抽出
+	refreshToken, err := user.DecodeRefreshToken(input.RefreshToken)
+	if err != nil {
+		return nil, err
+	}
+	// 有効なリフレッシュトークンか確認
+	err = u.tokenRepository.IsWhitelisted(refreshToken.Id)
+	if err != nil {
+		return nil, err
+	}
+	// アクセストークン発行
+	accessToken, err := user.NewAccessToken(refreshToken.UserId)
+	if err != nil {
+		return nil, err
+	}
+	return &TokenRefreshOutput{
+		AccessToken: accessToken.Token,
+		ExpiresIn:   accessToken.ExpiresIn,
+	}, nil
+}
