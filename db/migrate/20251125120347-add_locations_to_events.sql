5e719b84d8fb85b32f54bf74e1cd1d06d1057ddf
yonagi <lecterkn@proton.me>
2025-11-25 13:03:04 +0900

    fix: スキーマを修正 #77

--- db/migrate/20251125120347-add_locations_to_events.sql
+++ db/migrate/20251125120347-add_locations_to_events.sql
@@ -2,8 +2,8 @@
 -- +migrate Up
 CREATE TABLE event_locations(
     event_id UUID NOT NULL UNIQUE REFERENCES events(id) ON DELETE CASCADE,
-    location VARCHAR(255) NOT NULL,
-)
+    location VARCHAR(255) NOT NULL
+);
 
 CREATE UNIQUE INDEX idx_event_locations_event_id_and_location ON event_locations(event_id, location);
 CREATE INDEX idx_event_locations_event_id ON event_locations(event_id);
