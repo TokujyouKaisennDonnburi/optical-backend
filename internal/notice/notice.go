c057ed612b99f95c5bb8ea081b2169b78dfa3b22
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2025-11-25 14:02:17 +0900

    feat: 通知ドメインモデル #9

--- internal/notice/notice.go
+++ internal/notice/notice.go
@@ -1,1 +1,32 @@
 package notice
+
+// ドメインモデル
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Notice struct {
+	Id        uuid.UUID
+	EventId   uuid.UUID
+	Title     string
+	Content   string
+	IsRead    bool
+	CreatedAt time.Time
+}
+
+func NewNotice(eventID uuid.UUID, title, content string) (*Notice, error) {
+	id, err := uuid.NewV7()
+	if err != nil {
+		return nil, err
+	}
+	return &Notice{
+		Id:        id,
+		EventId:   eventID,
+		Title:     title,
+		Content:   content,
+		IsRead:    false,
+		CreatedAt: time.Now(),
+	}, nil
+}
