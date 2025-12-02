c9aa286b99ea7e83cc76e6a987a54e8b08e1919d
mame77 <morinaga.kota@icloud.com>
2025-12-02 14:02:56 +0900

    feat:wip

---
+++ internal/calendar/service/query/get_calendar.go
@@ -0,0 +1,51 @@
+package query
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar"
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/option"
+)
+
+type CalendarQueryInput struct {
+	CalendarId uuid.UUID
+	UserId     uuid.UUID
+}
+
+type CalendarQueryOutput struct {
+	Id      uuid.UUID
+	Name    string
+	Color   string
+	Image   calendar.Image
+	Members []calendar.Member
+	Options []option.Option
+}
+
+// カレンダー情報を取得
+func (q *CalendarQuery) GetCalendar(ctx context.Context, input CalendarQueryInput)(*CalendarQueryOutput, error) {
+	calendar, err := q.calendarRepository.FindById(ctx, input.CalendarId)
+	if err != nil {
+		return nil, err
+	}
+	isMember := false
+	for _, m := range calendar.Members {
+		if m.UserId == input.UserId{
+			isMember = true
+			break
+		}
+	}
+	if isMember == false {
+		return nil, errors.New("user is not in members")
+	}
+	return &CalendarQueryOutput{
+		Id:      calendar.Id,
+		Name:    calendar.Name,
+		Color:   calendar.Color,
+		Image:   calendar.Image,
+		Members: calendar.Members,
+		Options: calendar.Options,
+	}, nil
+
+} 
