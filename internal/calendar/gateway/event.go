3700d706b0082f4ef946420786124e43b78eb28a
yonagi <lecterkn@proton.me>
2025-12-02 11:30:29 +0900

    add: 予定更新用のリポジトリを実装 #22

--- internal/calendar/gateway/event.go
+++ internal/calendar/gateway/event.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar"
 	"github.com/TokujouKaisenDonburi/optical-backend/pkg/db"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/psql"
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 )
@@ -35,7 +36,7 @@ func (r *EventPsqlRepository) Create(
 			return err
 		}
 		query := `
-			INSERT INTO events(id, calendar_id, title, memo, color, all_day, start_at, end_at, created_at, updated_at)
+			INSERT INTO events(id, calendar_id, title, memo, location, color, all_day, start_at, end_at, created_at, updated_at)
 			VALUES(:id, :calendarId, :title, :memo, :color, :allDay, :startAt, :endAt, :createdAt, :updatedAt)
 		`
 		_, err = tx.NamedExecContext(ctx, query, map[string]any{
@@ -44,11 +45,51 @@ func (r *EventPsqlRepository) Create(
 			"title":      event.Title,
 			"memo":       event.Memo,
 			"color":      event.Color,
+			"location":   event.Location,
 			"allDay":     event.ScheduledTime.AllDay,
 			"startAt":    event.ScheduledTime.StartTime,
 			"endAt":      event.ScheduledTime.EndTime,
 			"createdAt":  time.Now(),
 			"updatedAt":  time.Now(),
+		})
+		return err
+	})
+}
+
+func (r *EventPsqlRepository) Update(
+	ctx context.Context,
+	eventId uuid.UUID,
+	updateFn func(event *calendar.Event) (*calendar.Event, error),
+) error {
+	return db.RunInTx(r.db, func(tx *sqlx.Tx) error {
+		event, err := psql.FindEventById(ctx, tx, eventId)
+		if err != nil {
+			return err
+		}
+		event, err = updateFn(event)
+		if err != nil {
+			return err
+		}
+		query := `
+			UPDATE event SET
+				title = :title,
+				memo = :memo,
+				color = :color,
+				allDay = :allDay,
+				start_at = :startAt,
+				end_at = :endAt
+			WHERE 
+				id = :id AND deleted_at IS NULL
+		`
+		_, err = tx.NamedExecContext(ctx, query, map[string]any{
+			"id":       event.Id,
+			"title":    event.Title,
+			"memo":     event.Memo,
+			"color":    event.Color,
+			"location": event.Location,
+			"allDay":   event.ScheduledTime.AllDay,
+			"startAt":  event.ScheduledTime.StartTime,
+			"endAt":    event.ScheduledTime.EndTime,
 		})
 		return err
 	})
