a2e0e4688cb2704cf0c210df5c6ef5d420837827
OMOCHInoHOSHI <166191100+OMOCHInoHOSHI@users.noreply.github.com>
2025-12-03 02:03:22 +0900

    feat: output定義 #18

---
+++ internal/calendar/service/query/output/event.go
@@ -0,0 +1,18 @@
+package output
+
+import (
+	"github.com/google/uuid"
+)
+
+// イベント取得で渡す出力データ
+type EventQueryOutput struct {
+	Id         uuid.UUID
+	CalendarId uuid.UUID
+	Title      string
+	Memo       string
+	Color      string
+	AllDay     bool
+	StartAt    string
+	EndAt      string
+	CreatedAt  string
+}
