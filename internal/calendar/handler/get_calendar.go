8f7b91f268565f021e860200c6315f6497b52cee
mame77 <morinaga.kota@icloud.com>
2025-12-02 14:29:40 +0900

    feat:wip

--- internal/calendar/handler/get_calendar.go
+++ internal/calendar/handler/get_calendar.go
@@ -1,15 +1,15 @@
 package handler
 
 import (
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar"
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/option"
 )
 
-
 type CalendarResponse struct {
 	Id		string `json:"id"`
-	Name    string
-	Color   string
-	Image   string
-	Members []calrendar.
-	Options []option.Option
+	Name    string `json:"name"`
+	Color   string `json:"color"`
+	Image   string `json:"image"`
+	Members []calendar.Member `json:"member"`
+	Options []option.Option     `json:"option"`
 }
