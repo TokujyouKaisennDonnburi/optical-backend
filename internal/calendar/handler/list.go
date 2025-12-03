5f940f15d2a9469aca6648406fcf537963c868a1
yonagi <lecterkn@proton.me>
2025-12-03 09:40:27 +0900

    fix: GetIdFromContextの移行漏れ #22

--- internal/calendar/handler/list.go
+++ internal/calendar/handler/list.go
@@ -4,9 +4,9 @@ import (
 	"net/http"
 
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar/service/query"
-	"github.com/TokujouKaisenDonburi/optical-backend/internal/user/handler"
 	"github.com/TokujouKaisenDonburi/optical-backend/pkg/apperr"
 	"github.com/TokujouKaisenDonburi/optical-backend/pkg/storage"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/auth"
 	"github.com/go-chi/render"
 )
 
@@ -24,7 +24,7 @@ type CalendarResponse struct {
 // カレンダー一覧を取得する
 func (h *CalendarHttpHandler) GetCalendars(w http.ResponseWriter, r *http.Request) {
 	// ユーザーIDを取得
-	userId, err := handler.GetUserIdFromContext(r)
+	userId, err := auth.GetUserIdFromContext(r)
 	if err != nil {
 		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
 		return
