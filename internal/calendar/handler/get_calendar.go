a0c656e1de288ebb0ec2530daa87de7201e2f578
mame77 <morinaga.kota@icloud.com>
2025-12-03 09:55:55 +0900

    feat:handlerの作成 #14

--- internal/calendar/handler/get_calendar.go
+++ internal/calendar/handler/get_calendar.go
@@ -4,7 +4,14 @@ import (
 	"net/http"
 
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar"
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar/service/query"
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/option"
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/user/handler"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/apperr"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+
+	"github.com/google/uuid"
 )
 
 type CalendarResponse struct {
@@ -23,20 +30,29 @@ func (h *CalendarHttpHandler) GetCalendar(w http.ResponseWriter, r *http.Request
 		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
 		return
 	}
-	calendarId, err := handler.GetCalendarIdFromContext(r)
+	// get calendarId
+	calendarId, err := uuid.Parse(chi.URLParam(r, "calendarId"))
 	if err != nil {
 		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
 		return
 	}
-
-	
+	// input
+	output, err := h.calendarQuery.GetCalendar(r.Context(), query.CalendarQueryInput{
+		UserId:     userId,
+		CalendarId: calendarId,
+	})
+	if err != nil {
+		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
+		return
+	}
+	// output
 	response := CalendarResponse{
-		Id:
-		Name:
-		Color:
-		Image:
-		Members:
-		Options:
+		Id:		 string(output.Id),
+		Name:	 output.Name,
+		Color:	 output.Color,
+		Image:	 output.Image.Url,
+		Members: output.Members,
+		Options: output.Options,
 	}
 	render.JSON(w,r,response)
 }
