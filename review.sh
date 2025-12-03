7f7b6b87d3c3feced20c91c37fc556ad3a28a9e2
yonagi <lecterkn@proton.me>
2025-12-03 13:47:41 +0900

    update: Agentの強化 #110

--- review.sh
+++ review.sh
@@ -4,6 +4,7 @@ if [ ! -d "tmp" ]; then
     mkdir "tmp"
 fi
 
-opencode run --agent=review "review $(git diff origin/main)" > tmp/ai-review.md
+git fetch origin main
+opencode run --agent=review "$(git diff origin/main)" > tmp/ai-review.md
 
 less tmp/ai-review.md
