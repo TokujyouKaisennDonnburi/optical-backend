64080eed75ce928328b5c5d15b2500a315ab56ae
yonagi <lecterkn@proton.me>
2025-12-03 11:31:33 +0900

    fix: コメント追加アクションを設定 #110

--- review.sh
+++ review.sh
@@ -6,6 +6,6 @@ if [ ! -d "tmp" ]; then
     mkdir "tmp"
 fi
 
-opencode run --agent=review "review $(git diff $(git merge-base HEAD origin/main))" > tmp/ai-review.md
+opencode run --agent=review "review $(git diff origin/main)" > tmp/ai-review.md
 
 less tmp/ai-review.md
