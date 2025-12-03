34aafa2cf7ff8c40c2cc279b0c783caed7577044
mame77 <morinaga.kota@icloud.com>
2025-12-04 00:33:24 +0900

    refactor:TODOコメントの追加 #26

--- internal/calendar/service/command/create_members.go
+++ internal/calendar/service/command/create_members.go
@@ -13,10 +13,18 @@ type MemberCreateInput struct {
 }
 
 func (c *CalendarCommand) CreateMember (ctx context.Context, input MemberCreateInput) (*calendar.Member, error) {
+	// TODO: c.memberRepository.Create の引数を修正する
+	//   Create(ctx, input.CalendarId, input.Email) の3つの引数が必要
+
+	// TODO: Createの戻り値を受け取って使う（今は _ で捨てている）
+	//   member, err := c.memberRepository.Create(...) として受け取る
+
+	// TODO: 受け取ったmemberを返す（今のnewMemberは使われていない）
+
 	var newMember *calendar.Member
 	_, err := c.memberRepository.Create(input.Email)
 	if err != nil {
-		return nil, err 
+		return nil, err
 	}
 	return newMember, nil
 }
