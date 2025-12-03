a83e784d88c107ab4cc17a6fac72089483afaa1d
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2025-12-03 16:40:35 +0900

    feat: Handler層 event #18

--- internal/calendar/handler/http.go
+++ internal/calendar/handler/http.go
@@ -1,15 +1,20 @@
 package handler
 
-import "github.com/TokujouKaisenDonburi/optical-backend/internal/calendar/service/command"
+import (
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar/service/command"
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar/service/query"
+)
 
 type CalendarHttpHandler struct {
 	eventCommand    *command.EventCommand
 	calendarCommand *command.CalendarCommand
+	eventQuery      *query.EventQuery
 }
 
 func NewCalendarHttpHandler(
 	eventCommand *command.EventCommand,
 	calendarCommand *command.CalendarCommand,
+	eventQuery *query.EventQuery,
 ) *CalendarHttpHandler {
 	if eventCommand == nil {
 		panic("EventCommand is nil")
@@ -17,8 +22,12 @@ func NewCalendarHttpHandler(
 	if calendarCommand == nil {
 		panic("CreateSchedule is nil")
 	}
+	if eventQuery == nil {
+		panic("EventQuery is nil")
+	}
 	return &CalendarHttpHandler{
 		eventCommand:    eventCommand,
 		calendarCommand: calendarCommand,
+		eventQuery:      eventQuery,
 	}
 }
---
+++ internal/calendar/handler/list_get_event.go
@@ -0,0 +1,44 @@
+package handler
+
+// GET /calendars/{calendarId}/events
+
+import (
+	"net/http"
+
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar/service/query"
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/user/handler"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/apperr"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"github.com/google/uuid"
+)
+
+// GET /calendars/{calendarId}/events
+func (h *CalendarHttpHandler) ListGetEvents(w http.ResponseWriter, r *http.Request) {
+	// 1. JWTからuserIDを取得
+	userId, err := handler.GetUserIdFromContext(r)
+	if err != nil {
+		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
+		return
+	}
+
+	// 2. パスパラメータからcalendarIDを取得
+	calendarId, err := uuid.Parse(chi.URLParam(r, "calendarId"))
+	if err != nil {
+		_ = render.Render(w, r, apperr.ErrInvalidRequest(err))
+		return
+	}
+
+	// 3. Queryを実行
+	events, err := h.eventQuery.ListGetEvents(r.Context(), query.EventQueryInput{
+		CalendarID: calendarId,
+		UserID:     userId,
+	})
+	if err != nil {
+		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
+		return
+	}
+
+	// 4. レスポンスを返す
+	render.JSON(w, r, events)
+}
