9f2b79cd6f91b7337e2e673ec5c41717546d8557
mame77 <morinaga.kota@icloud.com>
2025-12-04 12:14:54 +0900

    feat:inputの変更 #26

--- internal/calendar/repository/member.go
+++ internal/calendar/repository/member.go
@@ -1,11 +1,10 @@
 package repository
 
 import (
-	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar"
 	"github.com/google/uuid"
 	"golang.org/x/net/context"
 )
 
 type MemberRepository interface {
-	Create(ctx context.Context, calendarId uuid.UUID, email string)(calendar.Member, error)
+	Create(ctx context.Context, userId, calendarId uuid.UUID, email string)error
 }
