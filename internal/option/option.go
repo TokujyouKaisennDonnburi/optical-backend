fc0e9f775dab492bfff0ef504a167f73d4403fd2
mame77 <morinaga.kota@icloud.com>
2025-12-11 06:29:17 +0900

    feat:ドメインを変更 #21

--- internal/option/option.go
+++ internal/option/option.go
@@ -1,26 +1,21 @@
 package option
 
-import (
-	"errors"
-
-	"github.com/google/uuid"
-)
-
 type Option struct {
-	Id   uuid.UUID
+	Id   int
 	Name string
+	deprecated bool
 }
 
-func NewOption(name string) (*Option, error) {
-	id, err := uuid.NewV7()
-	if err != nil {
-		return nil, err
-	}
-	if name == "" {
-		return nil, errors.New("Option `Name` is empty")
-	}
-	return &Option{
-		Id:   id,
-		Name: name,
-	}, nil
-}
+// func NewOption(name string) (*Option, error) {
+// 	id, err := uuid.NewV7()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	if name == "" {
+// 		return nil, errors.New("Option `Name` is empty")
+// 	}
+// 	return &Option{
+// 		Id:   id,
+// 		Name: name,
+// 	}, nil
+// }
