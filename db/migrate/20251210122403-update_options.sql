8c0936a2fdcb50c54ce2017e0b6ca2dc2c3e6f5f
mame77 <morinaga.kota@icloud.com>
2025-12-11 05:31:09 +0900

    fix:downgradeの追加とdeprecatedの追加 #21

--- db/migrate/20251210122403-update_options.sql
+++ db/migrate/20251210122403-update_options.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS options;
 
 CREATE TABLE options(
     id INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    deprecated BOOLEAN DEFAULT FALSE
 );
 COMMENT ON COLUMN options.id IS 'オプションID';
 COMMENT ON COLUMN options.name IS 'オプション名';
@@ -16,5 +17,22 @@ CREATE TABLE calendar_options(
 );
 COMMENT ON COLUMN calendar_options.calendar_id IS 'カレンダーID';
 COMMENT ON COLUMN calendar_options.option_id IS 'オプションID';
+
 -- +migrate Down
+
+  DROP TABLE IF EXISTS calendar_options;
+  DROP TABLE IF EXISTS options;
+
+  CREATE TABLE options(
+      id UUID PRIMARY KEY,
+      name VARCHAR(255) NOT NULL
+  );
+
+  CREATE TABLE calendar_options(
+      calendar_id UUID REFERENCES calendars(id) ON
+   DELETE CASCADE,
+      option_id UUID REFERENCES options(id) ON
+  DELETE CASCADE,
+      PRIMARY KEY (calendar_id, option_id)
+  );
 
