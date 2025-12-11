31be20b287290e016a863d14406d08934aea0856
yonagi <lecterkn@proton.me>
2025-12-11 15:07:18 +0900

    add: github用テーブルを追加 #106

---
+++ db/migrate/20251205094614-create_github_ids.sql
@@ -0,0 +1,25 @@
+
+-- +migrate Up
+
+-- for GitHub SSO
+CREATE TABLE user_githubs(
+    user_id UUID PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+    github_id VARCHAR(255) NOT NULL,
+    github_name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- for GitHub Apps
+CREATE TABLE calendar_githubs(
+    calendar_id UUID PRIMARY KEY REFERENCES calendars(id) ON DELETE CASCADE,
+    github_id VARCHAR(255) NOT NULL,
+    github_name VARCHAR(255) NOT NULL,
+    installation_id VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +migrate Down
+DROP TABLE user_githubs;
+DROP TABLE calendar_githubs;
