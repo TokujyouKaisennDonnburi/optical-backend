403552ec481c342697aa58ed8ffcfa8246ef8075
mame77 <morinaga.kota@icloud.com>
2025-12-12 10:56:25 +0900

    feat:Input Outputを教えて #21

--- internal/option/service/query/get_list.go
+++ internal/option/service/query/get_list.go
@@ -3,7 +3,6 @@ package query
 import (
 	"context"
 
-	"github.com/TokujouKaisenDonburi/optical-backend/internal/option"
 	"github.com/google/uuid"
 )
 
@@ -12,18 +11,18 @@ type OptionListQueryInput struct {
 }
 
 type OptionListQueryOutput struct {
-	Id         uuid.UUID
+	Id         int32
 	Name       string
 	Deprecated bool
 }
 
 // option 一覧取得
-func (q *OptionQuery) GetListOption(ctx context.Context, input OptionListQueryInput) (OptionListQueryOutput, error) {
+func (q *OptionQuery) GetListOption(ctx context.Context, input OptionListQueryInput) ([]OptionListQueryOutput, error) {
 	outputs, err := q.optionRepository.GetList(ctx, input.UserId)
 	if err != nil {
 		return nil, err
 	}
-	result := make([]option.Option, len(outputs))
+	result := make([]OptionListQueryOutput, len(outputs))
 	for i, o := range outputs {
 		result[i] = OptionListQueryOutput{
 			Id:         o.Id,
