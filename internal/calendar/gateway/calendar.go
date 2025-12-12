fe55280990607c4e1257aa02b6a24dbf7915cb76
mame77 <morinaga.kota@icloud.com>
2025-12-12 13:55:59 +0900

    feat:queryの位置を変更 #14

--- internal/calendar/gateway/calendar.go
+++ internal/calendar/gateway/calendar.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar"
-	"github.com/TokujouKaisenDonburi/optical-backend/internal/calendar/service/query/output"
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/option"
 	"github.com/TokujouKaisenDonburi/optical-backend/pkg/db"
 	"github.com/TokujouKaisenDonburi/optical-backend/pkg/psql"
@@ -121,85 +120,5 @@ func (r *CalendarPsqlRepository) Create(
 		}
 		return nil
 	})
-}
-
-type CalendarListQueryModel struct {
-	Id       uuid.UUID      `db:"id"`
-	Name     string         `db:"name"`
-	Color    string         `db:"color"`
-	ImageId  uuid.NullUUID  `db:"image_id"`
-	ImageUrl sql.NullString `db:"image_url"`
-}
-
-// ユーザーが所属するカレンダー一覧を取得する
-func (r *CalendarPsqlRepository) FindByUserId(ctx context.Context, userId uuid.UUID) ([]output.CalendarListQueryOutput, error) {
-	query := `
-		SELECT 
-			c.id, c.name, c.color, c.image_id, ci.url AS image_url
-		FROM calendars c
-		INNER JOIN calendar_members m 
-			ON c.id = m.calendar_id
-		LEFT JOIN calendar_images ci
-			ON c.image_id = ci.id
-		WHERE 
-			m.user_id = $1
-			AND c.deleted_at IS NULL
-		ORDER BY c.id
-	`
-	var rows []CalendarListQueryModel
-	err := r.db.SelectContext(ctx, &rows, query, userId)
-	if err != nil {
-		return nil, err
-	}
-
-	calendars := make([]output.CalendarListQueryOutput, len(rows))
-	for i, row := range rows {
-		calendars[i] = output.CalendarListQueryOutput{
-			Id:    row.Id,
-			Name:  row.Name,
-			Color: row.Color,
-			Image: calendar.Image{
-				Id:    row.ImageId.UUID,
-				Url:   row.ImageUrl.String,
-				Valid: row.ImageId.Valid && row.ImageUrl.Valid,
-			},
-		}
-	}
-	return calendars, nil
-}
-
-type CalendarQueryModel struct {
-	Id      uuid.UUID         `db:"id"`
-	Name    string            `db:"name"`
-	Color   calendar.Color    `db:"color"`
-	Image   calendar.Image    `db:"image"`
-	Members []calendar.Member `db:"member"`
-	Options []option.Option   `db:"option"`
-}
-
-// calendarの単体取得
-func (r *CalendarPsqlRepository) FindById(ctx context.Context, id uuid.UUID) (*calendar.Calendar, error) {
-	query := `
-        SELECT c.id, c.name, c.color, i.id AS image_id, i.url, m.user_id, o.option_id
-        FROM calendars c
-		LEFT JOIN calendar_images i ON i.id = c.image_id
-		INNER JOIN calendar_options o ON o.calendar_id = c.id
-		INNER JOIN calendar_members m ON m.calendar_id = c.id
-        WHERE c.id = $1
-		ORDER BY c.id
-    `
-	model := CalendarQueryModel{}
-	err := r.db.SelectContext(ctx, &model, query, id)
-	if err != nil {
-		return nil, err
-	}
-	return &calendar.Calendar{
-		Id:      model.Id,
-		Name:    model.Name,
-		Color:   model.Color,
-		Image:   model.Image,
-		Members: model.Members,
-		Options: model.Options,
-	}, nil
 }
 
