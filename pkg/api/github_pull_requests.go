7f3b1f86594f60f04bd9ebffc3ebd37a7bb320f7
ApoRia432 <lecterkn@proton.me>
2025-12-14 01:10:59 +0900

    add: Githubプルリクエスト関連APIを追加 #152

---
+++ pkg/api/github_pull_requests.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/github"
+)
+
+func GetPullRequests(ctx context.Context, accessToken, owner, repos string) ([]github.PullRequest, error) {
+	client := http.Client{}
+	requestUrl := GITHUB_BASE_URL+"/repos/"+owner+"/"+repos+"/pulls"
+	requestUrl += "?per_page=100"
+	// インストール済みアプリ取得リクエスト
+	req, err := http.NewRequestWithContext(
+		ctx,
+		"GET",
+		requestUrl,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Accept", "application/vnd.github+json")
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+	req.Header.Add("X-GitHub-Api-Version", GITHUB_API_VERSION)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to get github pullrequests: %d", resp.StatusCode)
+	}
+	// WARNING: ドメインから直接参照
+	var pullRequests []github.PullRequest
+	if err := json.NewDecoder(resp.Body).Decode(&pullRequests); err != nil {
+		return nil, err
+	}
+	return pullRequests, nil
+}
