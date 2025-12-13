19da0ab5bd55b2f9d091fd83afa1cbec5a10ad7a
ApoRia432 <lecterkn@proton.me>
2025-12-14 01:14:10 +0900

    feat: プルリクエスト取得用ハンドラを実装 #152

--- internal/github/handler/handler.go
+++ internal/github/handler/handler.go
@@ -1,18 +1,27 @@
 package handler
 
-import "github.com/TokujouKaisenDonburi/optical-backend/internal/github/service/command"
+import (
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/github/service/command"
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/github/service/query"
+)
 
 type GithubHandler struct {
+	githubQuery   *query.GithubQuery
 	githubCommand *command.GithubCommand
 }
 
 func NewGithubHandler(
+	githubQuery *query.GithubQuery,
 	githubCommand *command.GithubCommand,
 ) *GithubHandler {
+	if githubQuery == nil {
+		panic("githubQuery is nil")
+	}
 	if githubCommand == nil {
 		panic("githubCommand is nil")
 	}
 	return &GithubHandler{
+		githubQuery:   githubQuery,
 		githubCommand: githubCommand,
 	}
 }
