3fd815cc6cd6ad9e3b34d7584ede7d77b60ae54d
ApoRia432 <lecterkn@proton.me>
2025-12-14 01:10:29 +0900

    add: Githubのモデルを追加 #152

---
+++ internal/github/pull_request.go
@@ -0,0 +1,18 @@
+package github
+
+const (
+	PULL_REQUEST_STATE_ALL   = "all"
+	PULL_REQUEST_STATE_OPEN  = "open"
+	PULL_REQUEST_STATE_CLOSE = "close"
+)
+
+type PullRequest struct {
+	Id             int64  `json:"id"`
+	Url            string `json:"html_url"`
+	Title          string `json:"title"`
+	State          string `json:"state"`
+	Number         int    `json:"number"`
+	Assignees      []User `json:"assignees"`
+	Reviewers      []User `json:"requested_reviewers"`
+	ReviewComments int    `json:"review_comments"`
+}
