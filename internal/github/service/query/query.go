e077a6b5c3a98d28f79b8e0f0720d411d8e466fc
ApoRia432 <lecterkn@proton.me>
2025-12-14 02:38:09 +0900

    feat: オプションが有効になっているかチェックする #152

--- internal/github/service/query/query.go
+++ internal/github/service/query/query.go
@@ -1,24 +1,33 @@
 package query
 
-import "github.com/TokujouKaisenDonburi/optical-backend/internal/github/repository"
+import (
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/github/repository"
+	optionRepo "github.com/TokujouKaisenDonburi/optical-backend/internal/option/repository"
+)
 
 type GithubQuery struct {
 	stateRepository  repository.StateRepository
+	optionRepository optionRepo.OptionRepository
 	githubRepository repository.GithubRepository
 }
 
 func NewGithubQuery(
 	stateRepository repository.StateRepository,
+	optionRepository optionRepo.OptionRepository,
 	githubRepository repository.GithubRepository,
 ) *GithubQuery {
 	if stateRepository == nil {
 		panic("stateRepository is nil")
+	}
+	if optionRepository == nil {
+		panic("optionRepository is nil")
 	}
 	if githubRepository == nil {
 		panic("githubRepository is nil")
 	}
 	return &GithubQuery{
 		stateRepository:  stateRepository,
+		optionRepository: optionRepository,
 		githubRepository: githubRepository,
 	}
 }
