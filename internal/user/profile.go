e89033d9d2ba3e5e4fc80368fa1c78fa7484aec3
yonagi <lecterkn@proton.me>
2025-12-15 12:23:10 +0900

    update: ドメインモデルの画像をアバターに変更 #160

--- internal/user/profile.go
+++ internal/user/profile.go
@@ -1,61 +1,32 @@
 package user
 
 import (
-	"errors"
 	"time"
-	"net/url"
 
 	"github.com/google/uuid"
-
 )
 
 type Profile struct {
 	Id        uuid.UUID
 	UserId    uuid.UUID
-	PhotoURL  string
+	Image     Avatar
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
-var (
-	ErrEmptyPhotoURL   = errors.New("photoURL is empty")
-	ErrInvalidPhotoURL = errors.New("photoURL validate error")
-)
-
 // NewProfile
-func NewProfile(userId uuid.UUID) (*Profile, error) {
+func NewProfile(userId uuid.UUID, imageUrl string) (*Profile, error) {
 	now := time.Now()
 	id, err := uuid.NewV7()
 	if err != nil {
 		return nil, err
 	}
+	img := NewAvatar(imageUrl)
 	return &Profile{
 		Id:        id,
 		UserId:    userId,
-		PhotoURL:  "",
+		Image:     *img,
 		CreatedAt: now,
 		UpdatedAt: now,
 	}, nil
-}
-
-// update photo
-func (p *Profile) UpdatePhotoURL(photoURL string) error {
-	if err := validatePhotoURL(photoURL); err != nil {
-		return err
-	}
-	p.PhotoURL = photoURL
-	p.UpdatedAt = time.Now()
-	return nil
-}
-
-// validate photoURL
-func validatePhotoURL(photoURL string) error {
-	if photoURL == "" {
-		return ErrEmptyPhotoURL
-	}
-	_, err := url.Parse(photoURL)
-	if err != nil {
-		return ErrInvalidPhotoURL
-	}
-	return nil
 }
