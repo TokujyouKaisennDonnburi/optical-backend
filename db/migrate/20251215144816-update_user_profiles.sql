1d086494d28af4d9db7d8904ab70c58f721d1892
ApoRia432 <lecterkn@proton.me>
2025-12-15 19:03:11 +0900

    fix: DOWNマイグレーションを修正 #166

--- db/migrate/20251215144816-update_user_profiles.sql
+++ db/migrate/20251215144816-update_user_profiles.sql
@@ -10,7 +10,7 @@ ALTER TABLE user_profiles ADD COLUMN avatar_id UUID REFERENCES avatars(id);
 
 -- +migrate Down
 
-ALTER TABLE user_profiles RENAME COLUMN avatar_id TO image_url 
-ALTER TABLE user_profiles ALTER COLUMN image_url TYPE TEXT NOT NULL;
+ALTER TABLE user_profiles DROP COLUMN avatar_id;
+ALTER TABLE user_profiles ADD COLUMN image_url TEXT NOT NULL;
 
 DROP TABLE avatars;
