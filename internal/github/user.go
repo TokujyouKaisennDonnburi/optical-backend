e965de7e2a441a5e7291eb1d1c03298a680e9ed1
ApoRia432 <lecterkn@proton.me>
2025-12-16 00:24:35 +0900

    fix: ユーザー名を取得できるように修正 #152

--- internal/github/user.go
+++ internal/github/user.go
@@ -2,7 +2,7 @@ package github
 
 type User struct {
 	Id        int64  `json:"id"`
-	Name      string `json:"name"`
+	Name      string `json:"login"`
 	Email     string `json:"email"`
 	Url       string `json:"html_url"`
 	AvatarUrl string `json:"avatar_url"`
