1b0d39e2d19b6271da69fefead75f7dde2ec900d
yonagi <lecterkn@proton.me>
2025-12-15 12:23:47 +0900

    update: プロフィール取得時に画像を取得 #160

--- internal/user/repository/user.go
+++ internal/user/repository/user.go
@@ -4,12 +4,13 @@ import (
 	"context"
 
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/user"
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/user/service/query/output"
 	"github.com/google/uuid"
 )
 
 type UserRepository interface {
-	Create (ctx context.Context, user *user.User) error
+	Create(ctx context.Context, user *user.User) error
 	FindById(ctx context.Context, id uuid.UUID) (*user.User, error)
+	FindProfileById(ctx context.Context, id uuid.UUID) (*output.UserQueryOutput, error)
 	FindByEmail(ctx context.Context, email string) (*user.User, error)
 }
-
