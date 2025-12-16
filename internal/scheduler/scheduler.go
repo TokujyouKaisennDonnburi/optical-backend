491aa363f79d394bae0fc1d32ea5155309263c8b
mame77 <morinaga.kota@icloud.com>
2025-12-16 23:53:03 +0900

    feat:method error #179

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -1,7 +1,9 @@
 package scheduler
 
 import (
+	"errors"
 	"time"
+	"unicode/utf8"
 
 	"github.com/google/uuid"
 )
@@ -13,20 +15,63 @@ type Scheduler struct {
 	Title      string
 	Memo       string
 	StartTime  time.Time
-	EndTitle   time.Time
+	EndTime   time.Time
 	IsAllDay   bool
 }
 
 type Scheduler_attendance struct {
 	Id        uuid.UUID
-	CalendaId uuid.UUID
+	CalendarId uuid.UUID
 	UserId    uuid.UUID
-	Coment    string
+	Comment    string
 }
+
 type Scheduler_status struct {
 	AttendanceId uuid.UUID
 	Time         time.Time
 	Status       int
 }
 
+func NewScheduler(userId, calendarId uuid.UUID, title, memo string, startTime, endTime time.Time, isAllDay bool) (*Scheduler, error) {
+	id, err := uuid.NewV7()
+	if err != nil {
+		return nil, err
+	}
+	if calendarId == uuid.Nil {
+		return nil, errors.New("calendarId is nil")
+	}
+	if userId == uuid.Nil {
+		return nil, errors.New("userId is nil")
+	}
+	err = Scheduler.SetTitle(title)
+	if err != nil {
+		return nil, err
+	}
+	return &Scheduler{
+		Id:         id,
+		CalendarId: calendarId,
+		UserId:     userId,
+		Title:      title,
+		Memo:       memo,
+		StartTime:  startTime,
+		EndTime:   endTime,
+		IsAllDay:   isAllDay,
+	}, nil
+}
+func (s *Scheduler) SetTitle(title string) error {
+	titleLength := utf8.RuneCountInString(title)
+	if titleLength < 1 || titleLength > 32 {
+		return errors.New("title is invalid")
+	}
+	s.Title = title
+	return nil
+}
+func (s *Scheduler) SetMemo(memo string) error {
+	memoLength := utf8.RuneCountInString(memo)
+	if memoLength < 1 || memoLength > 32 {
+		return errors.New("memo is invalid")
+	}
+	s.Memo = memo
+	return nil
+}
 
