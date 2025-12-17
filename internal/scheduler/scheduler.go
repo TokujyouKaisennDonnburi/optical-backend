0cfeb3ba1c0f4a54eea1047994743f5743dcdd95
mame77 <morinaga.kota@icloud.com>
2025-12-17 14:31:18 +0900

    feat:domain complete #179

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -19,14 +19,14 @@ type Scheduler struct {
 	IsAllDay   bool
 }
 
-type Scheduler_attendance struct {
-	Id         uuid.UUID
-	CalendarId uuid.UUID
-	UserId     uuid.UUID
-	Comment    string
+type SchedulerAttendance struct {
+	Id          uuid.UUID
+	SchedulerId uuid.UUID
+	UserId      uuid.UUID
+	Comment     string
 }
 
-type Scheduler_status struct {
+type SchedulerStatus struct {
 	AttendanceId uuid.UUID
 	Time         time.Time
 	Status       int
@@ -77,15 +77,68 @@ func (s *Scheduler) SetTitle(title string) error {
 }
 func (s *Scheduler) SetMemo(memo string) error {
 	memoLength := utf8.RuneCountInString(memo)
-	if memoLength < 1 || memoLength > 256 {
+	if memoLength > 256 {
 		return errors.New("memo is invalid")
 	}
 	s.Memo = memo
 	return nil
 }
 func (s *Scheduler) SetStartEndTime(startTime, endTime time.Time) error {
-	if startTime.After(endTime){
+	if startTime.After(endTime) {
 		return errors.New("")
+	}
+	return nil
+}
+
+func NewAttendance(id, schedulerId, userId uuid.UUID, comment string) error {
+	id, err := uuid.NewV7()
+	if err != nil {
+		return nil
+	}
+	if schedulerId == uuid.Nil {
+		return errors.New("schedulerId is nil")
+	}
+	if userId == uuid.Nil {
+		return errors.New("userId is nil")
+	}
+	s := &SchedulerAttendance{
+		Id:          id,
+		SchedulerId: schedulerId,
+		UserId:      userId,
+		Comment:     comment,
+	}
+	err = s.SetComment(comment)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *SchedulerAttendance) SetComment(comment string) error {
+	commentLength := len(comment)
+	if commentLength > 255 {
+		return nil
+	}
+	return nil
+}
+func NewStatus(attendanceId uuid.UUID, time time.Time, status int) error {
+	if attendanceId == uuid.Nil {
+		return errors.New("attendanceId is nil")
+	}
+	s := &SchedulerStatus{
+		AttendanceId: attendanceId,
+		Time:         time,
+		Status:       status,
+	}
+	err := s.SetStatus(status)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (s *SchedulerStatus) SetStatus(status int) error {
+	if status < 0 || status > 2 {
+		return errors.New("status is nil")
 	}
 	return nil
 }
