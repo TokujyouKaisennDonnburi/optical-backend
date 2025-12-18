82482660bf851175094f366e4a9f6397fb465c18
mame77 <morinaga.kota@icloud.com>
2025-12-18 14:57:19 +0900

    wip #180

--- internal/scheduler/repository/scheduler.go
+++ internal/scheduler/repository/scheduler.go
@@ -1,1 +1,17 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type Scheduler interface {
+	Create(
+		ctx context.Context,
+		id, calendarId uuid.UUID,
+		title, memo string,
+		startime, endtime uuid.Time,
+		isAllDay bool,
+	)error
+}
--- internal/scheduler/service/command/scheduler.go
+++ internal/scheduler/service/command/scheduler.go
@@ -1,1 +1,15 @@
 package command
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+type Response struct {
+	Id uuid.UUID
+}
+
+
+func SchedulerCreate (calendarId uuid.UUID, title, memo string, startTime, endTime time.Time, isAllDay bool)(*Response, error){
+	
+}
