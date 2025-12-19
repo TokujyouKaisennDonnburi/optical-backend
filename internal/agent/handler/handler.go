ae31f55862a218d59bd7b2b00d982486ed8ebeb4
yonagi <lecterkn@proton.me>
2025-12-19 12:52:10 +0900

    feat: ルーティングを設定 #184

--- internal/agent/handler/handler.go
+++ internal/agent/handler/handler.go
@@ -1,17 +1,27 @@
 package handler
 
 import (
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/agent/service/command"
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/agent/service/query"
 )
 
 type AgentHandler struct {
-	agentQuery *query.AgentQuery
+	agentQuery   *query.AgentQuery
+	agentCommand *command.AgentCommand
 }
 
 func NewAgentHandler(
 	agentQuery *query.AgentQuery,
+	agentCommand *command.AgentCommand,
 ) *AgentHandler {
+	if agentQuery == nil {
+		panic("agentQuery is nil")
+	}
+	if agentCommand == nil {
+		panic("agentCommand is nil")
+	}
 	return &AgentHandler{
-		agentQuery: agentQuery,
+		agentQuery:   agentQuery,
+		agentCommand: agentCommand,
 	}
 }
