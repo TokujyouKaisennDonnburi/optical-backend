f7b8fdbae91dd15bfad96c674573441ba84d479a
mame77 <morinaga.kota@icloud.com>
2025-12-21 05:26:29 +0900

    feat:handler の作成 #156

--- internal/github/handler/get_milestones.go
+++ internal/github/handler/get_milestones.go
@@ -3,22 +3,33 @@ package handler
 import (
 	"net/http"
 
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/apperr"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/auth"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 	"github.com/google/uuid"
 )
 
 type GithubMilestonesRequest struct {
-	UserId uuid.UUID
-	ClendarId uuid.UUID
+	UserId    uuid.UUID `json:"userId"`
+	ClendarId uuid.UUID `json:"calendarId"`
 }
 
 type GithubMilestonesResponse struct {
-	Title string
-	Progress int8
-	Open int8
-	Close int8
+	Title    string `json:"title"`
+	Progress int8   `json:"progress"`
+	Open     int8   `json:"open"`
+	Close    int8   `json:"close"`
 }
 
-func(h *GithubHandler) GetMilestones (w http.ResponseWriter, r *http.Request){
-	userId, err := uuid.Parse("")
+func (h *GithubHandler) GetMilestones(w http.ResponseWriter, r *http.Request) {
+	userId, err := auth.GetClientId("userId")
+	if err != nil {
+		return _  = render.Render(w, r, apperr.ErrInternalServerError(err))
+	}
+	calendarId, err := uuid.Parse(chi.URLParam(r, "calendarId"))
+	if err != nil {
+		return _ = render.Render(w, r, apperr.ErrInvalidRequest(err))
+	}
 
 }
