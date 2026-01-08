57d42638071aae630585176c73290d04d78b4710
mame77 <morinaga.kota@icloud.com>
2026-01-08 18:40:54 +0900

    refactor:gateway complete #180

--- internal/scheduler/gateway/create.go
+++ internal/scheduler/gateway/create.go
@@ -23,6 +23,23 @@ func NewSchedulerPsqlRepository(db *sqlx.DB) *SchedulerPsqlRepository {
 	}
 }
 
+type schedulerCreateParams struct {
+	Id         uuid.UUID `db:"id"`
+	CalendarId uuid.UUID `db:"calendarId"`
+	UserId     uuid.UUID `db:"userId"`
+	Title      string    `db:"title"`
+	Memo       string    `db:"memo"`
+	LimitTime  time.Time `db:"limitTime"`
+	IsAllDay   bool      `db:"isAllDay"`
+}
+
+type possibleDateParams struct {
+	SchedulerId uuid.UUID `db:"schedulerId"`
+	Date        time.Time `db:"date"`
+	StartTime   time.Time `db:"startTime"`
+	EndTime     time.Time `db:"endTime"`
+}
+
 // create
 func (r *SchedulerPsqlRepository) CreateScheduler(
 	ctx context.Context,
@@ -32,42 +49,48 @@ func (r *SchedulerPsqlRepository) CreateScheduler(
 	limitTime time.Time,
 	isAllDay bool,
 ) error {
-	err := db.RunInTx(r.db, func(tx *sqlx.Tx) error {
-		query := `
-			INSERT INTO scheduler(id, calendar_id, user_id, title, memo, limit_time, is_allday)
-			VALUES(:id, :calendarId, :userId, :title, :memo, :limitTime, :isAllDay)
-		`
-		_, err := tx.NamedExecContext(ctx, query, map[string]any{
-			"id":         id,
-			"calendarId": calendarId,
-			"userId":     userId,
-			"title":      title,
-			"memo":       memo,
-			"limitTime":  limitTime,
-			"isAllDay":   isAllDay,
-		})
+	return db.RunInTx(r.db, func(tx *sqlx.Tx) error {
+		// scheduler
+		sql := `
+		INSERT INTO scheduler(id, calendar_id, user_id, title, memo, limit_time, is_allday)
+		VALUES(:id, :calendarId, :userId, :title, :memo, :limitTime, :isAllDay)
+	`
+		schedulerParams := schedulerCreateParams{
+			Id:         id,
+			CalendarId: calendarId,
+			UserId:     userId,
+			Title:      title,
+			Memo:       memo,
+			LimitTime:  limitTime,
+			IsAllDay:   isAllDay,
+		}
+		_, err := tx.NamedExecContext(ctx, sql, schedulerParams)
 		if err != nil {
 			return err
 		}
-		query = `
-			INSERT INTO scheduler_possible_date(scheduler_id, date, start_time, end_time)
-			VALUES(:schedulerId, :date, :startTime, :endTime)
-		`
-		for _, possibleDate := range possibleDates {
-			_, err = tx.NamedExecContext(ctx, query, map[string]any{
-				"schedulerId": id,
-				"date":        possibleDate.Date,
-				"startTime":   possibleDate.StartTime,
-				"endTime":     possibleDate.EndTime,
-			})
-			if err != nil {
-				return err
+
+		// possibleDate
+		sql = `
+		INSERT INTO scheduler_possible_date(scheduler_id, date, start_time, end_time)
+		VALUES(:schedulerId, :date, :startTime, :endTime)
+	`
+		possibleDateParamsList := make([]possibleDateParams, len(possibleDates))
+		for i, possibleDate := range possibleDates {
+			possibleDateParamsList[i] = possibleDateParams{
+				SchedulerId: id,
+				Date:        possibleDate.Date,
+				StartTime:   possibleDate.StartTime,
+				EndTime:     possibleDate.EndTime,
 			}
+		}
+
+		if len(possibleDateParamsList) == 0 {
+			return nil
+		}
+		_, err = tx.NamedExecContext(ctx, sql, possibleDateParamsList)
+		if err != nil {
+			return err
 		}
 		return nil
 	})
-	if err != nil {
-		return err
-	}
-	return nil
 }
