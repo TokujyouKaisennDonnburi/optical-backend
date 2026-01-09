1316cdd79942551ecad74e2a468b310e5a2a0322
mame77 <morinaga.kota@icloud.com>
2026-01-09 13:33:53 +0900

    feat:all scheduler #230

---
+++ internal/scheduler/handler/get_attendance.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/scheduler/service/query"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/apperr"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/auth"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"github.com/google/uuid"
+)
+
+type AttendanceQueryRequest struct {
+	SchedulerId uuid.UUID `json:"schedulerId"`
+}
+type AttendanceQueryRespons struct {
+	Id           uuid.UUID              `json:"schedulerId"`
+	CalendarId   uuid.UUID              `json:"calendarId"`
+	UserId       uuid.UUID              `json:"userId"`
+	Title        string                 `json:"title"`
+	Memo         string                 `json:"memo"`
+	LimitTime    time.Time              `json:"limitTime"`
+	IsAllDay     bool                   `json:"isAllDay"`
+	PossibleDate []PossibleDateResponse `json:"possibleDate"`
+}
+type PossibleDateResponse struct {
+	Date      time.Time `json:"date"`
+	StartTime time.Time `json:"startTime"`
+	EndTime   time.Time `json:"endTime"`
+}
+
+func (h *SchedulerHttpHandler)GetAttendance(w http.ResponseWriter, r *http.Request){
+	// userId
+	userId, err := auth.GetUserIdFromContext(r)
+	if err != nil {
+		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
+		return
+	}
+	// calendarId
+	calendarId, err := uuid.Parse(chi.URLParam(r, "calendarId"))
+	if err != nil {
+		_ = render.Render(w, r, apperr.ErrInvalidRequest(err))
+		return
+	}
+	result, err := h.schedulerQuery.AttendanceQuery(ctx,query.AttendanceQueryInput{
+		SchedulerId: AttendanceQueryRequest.SchedulerId,
+		UserId: userId,
+		CalendarId: calendarId,
+	})
+}
---
+++ internal/scheduler/service/query/output/output.go
@@ -0,0 +1,24 @@
+package output
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type SchedulerAttendanceQuery struct {
+	Id            uuid.UUID
+	CalendarId    uuid.UUID
+	UserId        uuid.UUID
+	Title         string
+	Memo          string
+	LimitTime     time.Time
+	IsAllDay      bool
+	PossibleDate []PossibleDateOutput
+}
+type PossibleDateOutput struct {
+	Date      time.Time
+	StartTime time.Time
+	EndTime   time.Time
+}
+
