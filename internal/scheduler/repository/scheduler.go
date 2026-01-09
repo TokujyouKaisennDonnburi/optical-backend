23d9ef20b121f88ebb40b0769359d774eaf6b5f8
mame77 <morinaga.kota@icloud.com>
2026-01-09 13:33:53 +0900

    feat:all scheduler #181

--- internal/scheduler/repository/scheduler.go
+++ internal/scheduler/repository/scheduler.go
@@ -5,9 +5,11 @@ import (
 	"time"
 
 	"github.com/TokujouKaisenDonburi/optical-backend/internal/scheduler"
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/scheduler/service/query/output"
 	"github.com/google/uuid"
 )
 
 type SchedulerRepository interface {
-	CreateScheduler(ctx context.Context, id, calendarId, userId uuid.UUID, title, memo string, possibleDates []scheduler.PossibleDate, limitTime time.Time, isAllDay bool)error
+	CreateScheduler(ctx context.Context, id, calendarId, userId uuid.UUID, title, memo string, possibleDates []scheduler.PossibleDate, limitTime time.Time, isAllDay bool) error
+	FindById(ctx context.Context, id uuid.UUID) (output.SchedulerAttendanceQuery, error)
 }
