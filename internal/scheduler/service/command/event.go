0c82b785f6da3ca215ea07ac6d9dd9b44d74da33
mame77 <morinaga.kota@icloud.com>
2026-01-13 11:27:21 +0900

    command struct #234

---
+++ internal/scheduler/service/command/event.go
@@ -0,0 +1,14 @@
+package command
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type SchedulerEventInput struct {
+	date time.Time
+}
+
+func (s *SchedulerCommand)SchedulerEvent(ctx context.Context, )
