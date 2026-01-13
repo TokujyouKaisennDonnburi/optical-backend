e05cd424e18fc7af851903ae1c8e0b4646d74827
mame77 <morinaga.kota@icloud.com>
2026-01-13 13:52:14 +0900

    feat:event追加 #230

--- internal/scheduler/service/command/event.go
+++ internal/scheduler/service/command/event.go
@@ -4,11 +4,32 @@ import (
 	"context"
 	"time"
 
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/option"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/apperr"
 	"github.com/google/uuid"
 )
 
 type SchedulerEventInput struct {
-	date time.Time
+	CalendarId  uuid.UUID
+	SchedulerId uuid.UUID
+	UserId      uuid.UUID
+	Date        time.Time
 }
 
-func (s *SchedulerCommand)SchedulerEvent(ctx context.Context, )
+func (c *SchedulerCommand) SchedulerEvent(ctx context.Context, input SchedulerEventInput)error{
+		// option check
+	options, err := c.optionRepository.FindsByCalendarId(ctx, input.CalendarId)
+	if err != nil {
+		return err
+	}
+	hasOption := false
+	for _, x := range options {
+		if x.Id == option.OPTION_SCHEDULER {
+			hasOption = true
+			break
+		}
+	}
+	if !hasOption {
+		return apperr.ForbiddenError("option not enabled")
+	}
+}
