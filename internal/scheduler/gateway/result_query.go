19eb527aecabedbdd8a8fab711ae0e72e29e9e43
mame77 <morinaga.kota@icloud.com>
2026-01-14 11:46:52 +0900

    wip

--- internal/scheduler/gateway/result_query.go
+++ internal/scheduler/gateway/result_query.go
@@ -1,1 +1,27 @@
 package gateway
+
+import (
+	"context"
+	"time"
+
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/scheduler/service/query/output"
+	"github.com/google/uuid"
+)
+
+type ResultModel struct {
+	UserId    uuid.UUID `db:"user_id"`
+	Title     string    `db:"title"`
+	Memo      string    `db:"memo"`
+	LimitTime time.Time `db:"limit_time"`
+	IsAllDay  bool      `db:"is_allDay"`
+	Date      time.Time `db:"date"`
+	StartTime time.Time `db:"start_time"`
+	EndTime   time.Time `db:"end_time"`
+}
+type MemberModel struct {
+	UserId   uuid.UUID
+	UserName uuid.UUID
+}
+func (d *SchedulerPsqlRepository)ResultGateway(ctx context.Context, calendarId, schedulerId,userId uuid.UUID)(output.SchedulerResultOutput, error){
+	sql := SELECT scheduler.
+}
