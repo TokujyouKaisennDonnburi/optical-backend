6f41ce7cb94fe6ef37398f133e78bcf76e1a1217
lecterkn <lecterkn@proton.me>
2026-01-16 21:32:35 +0900

    fix: NULL制約エラー #268

--- db/migrate/20260116201159-drop_color_column_from_events.sql
+++ db/migrate/20260116201159-drop_color_column_from_events.sql
@@ -3,5 +3,5 @@
 ALTER TABLE events DROP COLUMN color;
 
 -- +migrate Down
-ALTER TABLE events ADD COLUMN color VARCHAR(50) NOT NULL;
+ALTER TABLE events ADD COLUMN color VARCHAR(50) NOT NULL DEFAULT '';
 COMMENT ON COLUMN events.color IS 'カラー';
