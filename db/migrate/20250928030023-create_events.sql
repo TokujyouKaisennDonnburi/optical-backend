afc4239be5093764c89de37c3ef12ba03765571b
lecterkn <lecterkn@proton.me>
2026-01-16 20:16:30 +0900

    fix: カラー削除のマイグレーションを修正 #268

--- db/migrate/20250928030023-create_events.sql
+++ db/migrate/20250928030023-create_events.sql
@@ -4,6 +4,7 @@ CREATE TABLE events(
     calendar_id UUID NOT NULL REFERENCES calendars(id) ON DELETE CASCADE,
     title VARCHAR(255) NOT NULL,
     memo TEXT NOT NULL,
+    color VARCHAR(50) NOT NULL,
     all_day BOOLEAN NOT NULL,
     start_at TIMESTAMPTZ NOT NULL,
     end_at TIMESTAMPTZ NOT NULL,
@@ -16,6 +17,7 @@ COMMENT ON COLUMN events.id IS 'イベントID';
 COMMENT ON COLUMN events.calendar_id IS 'カレンダーID';
 COMMENT ON COLUMN events.title IS 'タイトル';
 COMMENT ON COLUMN events.memo IS 'メモ';
+COMMENT ON COLUMN events.color IS 'カラー';
 COMMENT ON COLUMN events.all_day IS '終日フラグ';
 COMMENT ON COLUMN events.start_at IS '開始時刻';
 COMMENT ON COLUMN events.end_at IS '終了時刻';
