55901fcd8ce7b0d70ad458f30b6bc75d65c92ec4
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2026-01-18 05:04:58 +0900

    feat: repository #263

---
+++ internal/github/service/query/output/isinstalled_githubapp.go
@@ -0,0 +1,9 @@
+package output
+
+type IsInstalledGithubAppQueryOutput struct {
+	IsInstalled    bool
+	GithubId       string
+	GithubName     string
+	InstallationId string
+	InstalledAt    string
+}
