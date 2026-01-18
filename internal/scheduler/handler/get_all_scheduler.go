50822bad6e9b0ffa3db9a5d4c9f97ccc80c289ee
mame77 <morinaga.kota@icloud.com>
2026-01-18 10:37:01 +0900

    feat:handler complete #235

---
+++ internal/scheduler/handler/get_all_scheduler.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/TokujouKaisenDonburi/optical-backend/internal/scheduler/service/query"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/apperr"
+	"github.com/TokujouKaisenDonburi/optical-backend/pkg/auth"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"github.com/google/uuid"
+)
+
+type AllSchedulerResponse struct {
+	Id         uuid.UUID `json:"scheduler_id"`
+	CalendarId uuid.UUID `json:"calendar_id"`
+	UserId     uuid.UUID `json:"user_id"`
+	Title      string    `json:"title"`
+	Memo       string    `json:"memo"`
+	LimitTime  time.Time `json:"limit_time"`
+	IsAllDay   bool      `json:"is_allday"`
+	IsDone     bool      `json:"is_done"`
+}
+
+func (h *SchedulerHttpHandler) GetScheduler(w http.ResponseWriter, r *http.Request) {
+	calendarId, err := uuid.Parse(chi.URLParam(r, "calendarId"))
+	if err != nil {
+		_ = render.Render(w, r, apperr.ErrInvalidRequest(err))
+		return
+	}
+	// userId
+	userId, err := auth.GetUserIdFromContext(r)
+	if err != nil {
+		_ = render.Render(w, r, apperr.ErrInternalServerError(err))
+		return
+	}
+	output, err := h.schedulerQuery.SchedulerQuery(r.Context(), query.SchedulerInput{
+		CalendarId: calendarId,
+		UserId:     userId,
+	})
+	response := AllSchedulerResponse{
+		Id:         output.Id,
+		CalendarId: output.CalendarId,
+		UserId:     output.UserId,
+		Title:      output.Title,
+		Memo:       output.Memo,
+		LimitTime:  output.LimitTime,
+		IsAllDay:   output.IsAllDay,
+		IsDone:     output.IsDone,
+	}
+	render.JSON(w, r, response)
+}
