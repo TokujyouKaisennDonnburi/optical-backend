5c73ec0dd82b37628a989ee790bcb15c2dc060aa
mame77 <morinaga.kota@icloud.com>
2026-01-18 09:56:28 +0900

    feat:repository #235

--- internal/scheduler/repository/scheduler.go
+++ internal/scheduler/repository/scheduler.go
@@ -12,6 +12,7 @@ import (
 type SchedulerRepository interface {
 	CreateScheduler(ctx context.Context, id, calendarId, userId uuid.UUID, title, memo string, possibleDates []scheduler.PossibleDate, limitTime time.Time, isAllDay bool) error
 	FindSchedulerById(ctx context.Context, id uuid.UUID) (*output.SchedulerOutput, error)
+	FindAllSchedulerById(ctx context.Context, id uuid.UUID) (*scheduler.Scheduler, error)
 	AddAttendance(ctx context.Context, id, schedulerId, userId uuid.UUID, comment string, schedulerStatus []scheduler.SchedulerStatus) error
 	FindResultByIdAndUserId(ctx context.Context, schedulerId, userId uuid.UUID) (*output.SchedulerResultOutput, error)
 	FindAttendanceById(ctx context.Context, calendarId, schedulerId, userId uuid.UUID) (*output.SchedulerAttendanceOutput, error)
