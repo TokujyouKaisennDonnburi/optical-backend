23566dfa072d19f761f5b77dc1de9c7bfbb80d2d
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2026-01-29 10:36:32 +0900

    update: トランザクション対応 #315

--- internal/calendar/service/command/join_member.go
+++ internal/calendar/service/command/join_member.go
@@ -28,28 +28,29 @@ func (c *CalendarCommand) JoinMember(ctx context.Context, input CalendarJoinInpu
 
 // 個別トークンからカレンダーメンバーに追加
 func (c *CalendarCommand) JoinMemberWithToken(ctx context.Context, input JoinWithTokenInput) error {
-	// トークン検証
-	invitation, err := c.memberRepository.FindInvitationByToken(ctx, input.Token)
-	if err != nil {
-		return err
-	}
-	if invitation.CalendarId != input.CalendarId {
-		return apperr.ValidationError("invalid token for this calendar")
-	}
-	if invitation.IsExpired() {
-		return apperr.ValidationError("invitation expired")
-	}
-	if invitation.IsUsed() {
-		return apperr.ValidationError("invitation already used")
-	}
+	return c.transactor.Transact(ctx, func(ctx context.Context) error {
+		// トークン検証
+		invitation, err := c.memberRepository.FindInvitationByToken(ctx, input.Token)
+		if err != nil {
+			return err
+		}
+		if invitation.CalendarId != input.CalendarId {
+			return apperr.ValidationError("invalid token for this calendar")
+		}
+		if invitation.IsExpired() {
+			return apperr.ValidationError("invitation expired")
+		}
+		if invitation.IsUsed() {
+			return apperr.ValidationError("invitation already used")
+		}
 
-	// 招待使用済み更新
-	invitation.MarkAsUsed(input.UserId)
-	if err := c.memberRepository.MarkInvitationAsUsed(ctx, invitation); err != nil {
-		return err
-	}
+		// 招待使用済み更新
+		invitation.MarkAsUsed(input.UserId)
+		if err := c.memberRepository.MarkInvitationAsUsed(ctx, invitation); err != nil {
+			return err
+		}
 
-	// メンバー追加
-	return c.memberRepository.AddMember(ctx, input.CalendarId, input.UserId)
+		// メンバー追加
+		return c.memberRepository.AddMember(ctx, input.CalendarId, input.UserId)
+	})
 }
-
